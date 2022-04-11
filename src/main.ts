aa75bedf04404c507165cd24b8510ddc4cec8c48
Hannah Ellis <hannahellis4242@gmail.com>
2022-04-12 00:12:42 +0100

    Adding SelectCommand

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import GameState from "./controler/GameState";
+import SelectCommand from "./controler/SelectCommand";
 import StartCommand from "./controler/StartCommand";
+import Card from "./model/Card";
 import { createDeck } from "./model/Deck";
 
 const state = new GameState();
 state.add(new StartCommand(createDeck()));
+state.add(new SelectCommand(new Card("Cups", "Ace", false)));
+console.log(JSON.stringify(state.model, undefined, 4));
+state.add(new SelectCommand(new Card("Swords", "Ace", false)));
+console.log(JSON.stringify(state.model, undefined, 4));
+state.undo();
 console.log(JSON.stringify(state.model, undefined, 4));
