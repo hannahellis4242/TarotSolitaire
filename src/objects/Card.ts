dd15262dcf7d63c34a6bccd5fd8606c9086343b0
hannahellis4242 <hannahellis4242@gmail.com>
2022-04-21 16:09:10 +0100

    Adding Card Sprite

--- src/objects/Card.ts
+++
@@ -1,3 +0,0 @@
-/*export default class Card extends Phaser.GameObjects{
-
-}*/
---
+++ src/sprites/CardSprite.ts
@@ -0,0 +1,5 @@
+export default class CardSprite extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
+    super(scene, x, y, texture);
+  }
+}
