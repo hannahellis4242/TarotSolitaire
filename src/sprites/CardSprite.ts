262bcebc18905cf4418d50109c017068da4e7105
Hannah Ellis <hannahellis4242@gmail.com>
2022-04-22 06:16:54 +0100

    Adding some more detail to the card sprite

--- src/sprites/CardSprite.ts
+++ src/sprites/CardSprite.ts
@@ -1,5 +1,21 @@
+import { Slot } from "../utils/GameLayout";
+
 export default class CardSprite extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
-    super(scene, x, y, texture);
+  constructor(scene: Phaser.Scene, slot: Slot, texture: string) {
+    super(scene, slot.x, slot.y, texture);
+    this.scene.add
+      .rectangle(slot.x, slot.y, slot.width, slot.height, 0, 1)
+      .setOrigin(0);
+    const boarder = 1;
+    this.scene.add
+      .rectangle(
+        slot.x + boarder,
+        slot.y + boarder,
+        slot.width - 2 * boarder,
+        slot.height - 2 * boarder,
+        0xffffff,
+        1
+      )
+      .setOrigin(0);
   }
 }
