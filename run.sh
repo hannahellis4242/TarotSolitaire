4418bcd3f94bd84088b3d0290eef4bccc8c7c8ef
Hannah Ellis <hannahellis4242@gmail.com>
2022-04-23 06:42:52 +0100

    Adding card assets and adding a script to help with creation

---
+++ run.sh
@@ -0,0 +1,7 @@
+names=("0 The Fool" "I The Magician" "II The High Priestess" "III The Empress" "IV The Emperor" "V The Hierophant" "VI The Lovers" "VII The Chariot" "VIII Strength" "IX The Hermit" "X Wheel of Fortune" "XI Justice‡" "XII The Hanged Man" "XIII Death" "XIV Temperance" "XV The Devil" "XVI The Tower" "XVII The Star" "XVIII The Moon" "XIX The Sun" "XX Judgement" "XXI The World" "Ace of Wands" "Two of Wands" "Three of Wands" "Four of Wands" "Five of Wands" "Six of Wands" "Seven of Wands" "Eight of Wands" "Nine of Wands" "Ten of Wands" "Page of Wands" "Knight of Wands" "Queen of Wands" "King of Wands" "Ace of Cups" "Two of Cups" "Three of Cups" "Four of Cups" "Five of Cups" "Six of Cups" "Seven of Cups" "Eight of Cups" "Nine of Cups" "Ten of Cups" "Page of Cups" "Knight of Cups" "Queen of Cups" "King of Cups" "Ace of Swords" "Two of Swords" "Three of Swords" "Four of Swords" "Five of Swords" "Six of Swords" "Seven of Swords" "Eight of Swords" "Nine of Swords" "Ten of Swords" "Page of Swords" "Knight of Swords" "Queen of Swords" "King of Swords" "Ace of Pentacles" "Two of Pentacles" "Three of Pentacles" "Four of Pentacles" "Five of Pentacles" "Six of Pentacles" "Seven of Pentacles" "Eight of Pentacles" "Nine of Pentacles" "Ten of Pentacles" "Page of Pentacles" "Knight of Pentacles" "Queen of Pentacles" "King of Pentacles" );
+
+for i in $(seq 0 78)
+do
+    echo "${names[i]}"
+    #sed -i 's/Hello/Hello/g' "card_$i.svg"
+done
