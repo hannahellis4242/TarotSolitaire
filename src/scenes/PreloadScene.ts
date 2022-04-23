0d8b410b8b88dd523359a7cdebd15015dd398874
Hannah Ellis <hannahellis4242@gmail.com>
2022-04-23 04:08:30 +0100

    preload all card assets

--- src/scenes/PreloadScene.ts
+++ src/scenes/PreloadScene.ts
@@ -1,14 +1,19 @@
 import Phaser from "phaser";
+import { createDeck } from "../model/Deck";
 
 class PreloadScene extends Phaser.Scene {
   constructor() {
     super("PreloadScene");
   }
   preload() {
-    this.load.svg("penticle", "assets/penticle.svg");
-    this.load.svg("TwoSwords", "assets/TwoOfSwords.svg");
+    createDeck().forEach((card) => {
+      this.load.svg(
+        `card_${card.suit}_${card.pip}`,
+        `assets/card_${card.suit}_${card.pip}.svg`
+      );
+    });
+
     this.load.svg("back", "assets/back.svg");
-    this.load.svg("front", "assets/front.svg");
   }
   create() {
     this.scene.start("TitleScene");
